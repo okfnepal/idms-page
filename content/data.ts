cabb28376485b320294a60a21f5b3338efc56339
James Shrestha <zaymesshrestha@gmail.com>
2024-02-06 15:43:19 +0545

    static page data

---
+++ content/data.ts
@@ -0,0 +1,87 @@
+export const existingProblems = [
+    {
+        imageSrc: "/images/many_systems.png",
+        imageWidth: "50px",
+        title: "Many Digital Systems",
+        body: "Digital systems generate valuable data for governments, public institutions, and citizens; however, the lack of a proper mechanism hampers their effective utilization."
+    },
+    {
+        imageSrc: "/images/silos.png",
+        imageWidth: "50px",
+        title: "Data Silos",
+        body: "The absence of interconnected systems leads to repetitive data input in individual systems, creating isolated data silos."
+    },
+    {
+        imageSrc: "/images/standard.png",
+        imageWidth: "36px",
+        title: "Lack of Data Standarization",
+        body: "Datasets stored across different systems result in varied data and a lack of standardization."
+    },
+    {
+        imageSrc: "/images/complex_data.png",
+        imageWidth: "36px",
+        title: "Complex Data Utilization Mechanism",
+        body: "Current systems are inefficient in generating and providing data of public interest, contributing to a complex data utilization process."
+    }
+];
+  
+
+
+
+export const featuresList = [
+    {
+        imageSrc: "/images/features/datastore.svg",
+        alt: "datastore",
+        title: "Comprehensive Datastore",
+        body: "Provides comprehensive datastore features to enhance the current data management mechanisms."
+    },
+    {
+        imageSrc: "/images/features/interactive.svg",
+        alt: "datastore",
+        title: "Interactive Visualization",
+        body: "Enables interactive visualization and dashboards using stored datasets."
+    },
+    {
+        imageSrc: "/images/features/themeable.svg",
+        alt: "datastore",
+        title: "Themeable",
+        body: "Fully customizable to align with specific brand requirements."
+    },
+    {
+        imageSrc: "/images/features/publish.svg",
+        alt: "datastore",
+        title: "Publish and Manage Data",
+        body: "User-friendly web interface for publishers and curators to effortlessly register, update and manage datasets."
+    },
+    {
+        imageSrc: "/images/features/share.svg",
+        alt: "datastore",
+        title: "Share and Integrate",
+        body: "Facilitates easy access and sharing of datasets with the general public for utilization and integration."
+    },
+    {
+        imageSrc: "/images/features/search.svg",
+        alt: "datastore",
+        title: "Search and Discovery",
+        body: "Offers a comprehensive search experience with keyword search and filtering based on tags and categories."
+    },
+    {
+        imageSrc: "/images/features/api.svg",
+        alt: "datastore",
+        title: "Data API",
+        body: "Simplifies integration for all users, allowing external code to seamlessly utilize the data API."
+    },
+    {
+        imageSrc: "/images/features/metadata.svg",
+        alt: "datastore",
+        title: "Metadata",
+        body: "Automatically includes a robust set of metadata for each dataset, with the flexibility to add additional attributes as needed."
+    },
+    {
+        imageSrc: "/images/features/geodata.svg",
+        alt: "datastore",
+        title: "Geospatial",
+        body: "Encompassing advanced geospatial features that include data preview, search, and discovery."
+    }
+];
+
