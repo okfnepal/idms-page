83ae9ae18e4ab8dbe1db2b6fcb503b88a748e508
James Shrestha <zaymesshrestha@gmail.com>
2024-09-13 18:57:03 +0545

    added shuddhodhan IDMS in showcase page

--- components/Showcases.tsx
+++ components/Showcases.tsx
@@ -1,9 +1,10 @@
 
 const projects = [
-  { imagePath: "/images/Logo.png", url: "https://data.tulsipurmun.gov.np", heading: "Tulsipur Sub-Metropolitan City", body: "Integrated Data Management System (IDMS)" },
   { imagePath: "/images/Logo.png", url: "https://data.birgunjmun.gov.np", heading: "Birgunj Metropolitan City", body: "Integrated Data Management System (IDMS)" },
+  { imagePath: "/images/Logo.png", url: "https://data.tulsipurmun.gov.np", heading: "Tulsipur Sub-Metropolitan City", body: "Integrated Data Management System (IDMS)" },
+  { imagePath: "/images/Logo.png",url: "https://data.janakpurmun.gov.np", heading: "Janakpurdham Sub-Metropolitan City", body: "Integrated Data Management System (IDMS)" },
   { imagePath: "/images/Logo.png",url: "https://data.lekbeshimun.gov.np", heading: "Lekbeshi Municipality", body: "Integrated Data Management System (IDMS)" },
-  { imagePath: "/images/Logo.png",url: "https://data.janakpurmun.gov.np", heading: "Janakpurdham Sub-Metropolitan City", body: "Integrated Data Management System (IDMS)" }
+  { imagePath: "/images/Logo.png",url: "https://data.shuddhodhan.oknp.org/", heading: "Shuddhodhan Rural Municipality", body: "Integrated Data Management System (IDMS)" }
 
 ]
 
